8d8928c3860d9997819f97e098538a500427154a
Ajai Shivam <offajaishivam@gmail.com>
2023-08-24 14:17:58 +0530

    create a method of following defination public int product(int x, int y) {} call this method from Main using Product class object p

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,11 @@
 package com.driver;
 
 public class Main {
-    Product p = new Product();
+   Product p = new Product();
+   p.product(2,3);
 }
 class Product{
-
+    public int product(int x, int y) {
+        return 0;
+    }
 }
