d3c7c77508ebd9af2723cef94da3d186d54e7873
Ajai Shivam <offajaishivam@gmail.com>
2023-08-24 14:37:46 +0530

    creating function

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class Main {
    Product p = new Product();
-   p.product(2,3);
+
 }
 class Product{
     public int product(int x, int y) {
-        return 0;
+
     }
 }
